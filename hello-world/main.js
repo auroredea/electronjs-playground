2adfdf3b9a3a849e61463afe38e1c0e510f6ce06
Aurore <2429626+auroredea@users.noreply.github.com>
2025-05-20 23:06:12 +0200

    feat: update hello world application without theme

---
+++ hello-world/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 360,
+    height: 500,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
